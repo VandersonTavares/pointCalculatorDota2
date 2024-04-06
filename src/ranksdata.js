a102cb8392bbef977aa3ccdf9520e9206e94aebd
Vanderson Tavares <vandersontavares32@gmail.com>
2024-04-06 14:59:23 -0300

    feat: update rank raw data

--- src/ranksdata.js
+++ src/ranksdata.js
@@ -1,37 +1,59 @@
 const ranksData = {
-    "10-15": {
-      name: "Herald",
-      image: "link_da_imagem_herald.png"
-    },
-    "20-25": {
-      name: "Guardian",
-      image: "link_da_imagem_guardian.png"
-    },
-    "30-35": {
-      name: "Crusader",
-      image: "link_da_imagem_crusader.png"
-    },
-    "40-45": {
-      name: "Archon",
-      image: "link_da_imagem_archon.png"
-    },
-    "50-55": {
-      name: "Legend",
-      image: "link_da_imagem_legend.png"
-    },
-    "60-65": {
-      name: "Ancient",
-      image: "link_da_imagem_ancient.png"
-    },
-    "70-75": {
-      name: "Divine",
-      image: "link_da_imagem_divine.png"
-    },
-    "80-85": {
-      name: "Immortal",
-      image: "link_da_imagem_immortal.png"
-    }
-  };
-  
-  export default ranksData;
-  
+  "10-15 Herald": {
+    10: "rank1",
+    11: "rank2",
+    12: "rank3",
+    13: "rank4",
+    14: "rank5",
+  },
+  "20-25 Guardian": {
+    20: "rank1",
+    21: "rank2",
+    22: "rank3",
+    23: "rank4",
+    24: "rank5",
+  },
+  "30-35 Crusader": {
+    30: "rank1",
+    31: "rank2",
+    32: "rank3",
+    33: "rank4",
+    34: "rank5",
+  },
+  "40-45 Archon": {
+    40: "rank1",
+    41: "rank2",
+    42: "rank3",
+    43: "rank4",
+    44: "rank5",
+  },
+  "50-55 Legend": {
+    50: "rank1",
+    51: "rank2",
+    52: "rank3",
+    53: "rank4",
+    54: "rank5",
+  },
+  "60-65 Ancient": {
+    60: "rank1",
+    61: "rank2",
+    62: "rank3",
+    63: "rank4",
+    64: "rank5",
+  },
+  "70-75 Divine": {
+    70: "rank1",
+    71: "rank2",
+    72: "rank3",
+    73: "rank4",
+    74: "rank5",
+  },
+  "80-85 Immortal": {
+    80: "rank1",
+    81: "rank2",
+    82: "rank3",
+    83: "rank4",
+    84: "rank5",
+  }
+};
+export default ranksData;
